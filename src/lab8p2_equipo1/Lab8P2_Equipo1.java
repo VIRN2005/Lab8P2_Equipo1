e24cdee77788e1c26095754d60422623c33fb636
Víctor Romero <virn2005@gmail.com>
2023-03-17 13:56:22 -0600

    Inicio de GUI

---
+++ src/lab8p2_equipo1/Lab8P2_Equipo1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab8p2_equipo1;
+
+/**
+ *
+ * @author Hogar
+ */
+public class Lab8P2_Equipo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
